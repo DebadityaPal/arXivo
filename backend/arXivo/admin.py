b749fab56f442c0a175f0a49c6c85c2c49f091b2
DebadityaPal <debaditya.pal6@gmail.com>
2021-10-11 14:51:09 +0530

    Custom User Model added

--- backend/arXivo/admin.py
+++ backend/arXivo/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from arXivo.models import ArXivoUser
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+
+class ArXivoUserAdmin(UserAdmin):
+    model = ArXivoUser
+    list_display = ["email", "username", "isVerified"]
+
+
+admin.site.register(ArXivoUser, ArXivoUserAdmin)
--- backend/arXivo/models.py
+++ backend/arXivo/models.py
@@ -1,1 +1,10 @@
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class ArXivoUser(AbstractUser):
+    isVerified = models.BooleanField("Verified User", default=False)
+    email = models.EmailField("Email Address", blank=False, null=False, unique=True)
+
+    def __str__(self):
+        return self.username
